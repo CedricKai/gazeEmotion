46abadb0d3a81766b0665d8aed0b9bc39173fc79
CedricKai <1291357423@qq.com>
2026-03-23 17:35:00 +0800

    new framework structure

---
+++ models/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+import timm
+import torch
+from omegaconf import DictConfig
+from models.vgg import VGG
+
+def create_model(config: DictConfig) -> torch.nn.Module:
+    mode = config.mode
+    if mode in ['MPIIGaze', 'MPIIFaceGaze']:
+        module = importlib.import_module(
+            f'ptgaze.models.{mode.lower()}.{config.model.name}')
+        model = module.Model(config)
+    elif mode == 'ETH-XGaze':
+        model = timm.create_model(config.model.name, num_classes=2)
+    else:
+        raise ValueError
+    device = torch.device(config.device)
+    model.to(device)
+    return model
